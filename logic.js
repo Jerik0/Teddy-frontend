8ece8d8766d2d4139c2d0f2c94522f38cee58bf3
Jeriko <jcarrico1985@live.com>
2018-01-22 10:16:50 -0600

    change minor styling in style.css. working on logic for adding/removing flash class in logic.js

--- logic.js
+++ logic.js
@@ -5,13 +5,26 @@
       $(this).children().css({
         'color': '#ff4632',
       });
-      $(this).addClass('hovered');
+      $(this).addClass('hovered'); //change font color
     },
     'mouseleave': function() {
       $(this).children().css({
         'color': 'white'
       });
-      $(this).removeClass('hovered');
+      $(this).removeClass('hovered'); //change font color back
+    },
+    'click': function() {
+      let i = 0;
+
+      while(i < this.classList.length) {
+        console.log(i);
+        console.log(this.classList[i]);
+        if(this.classList[i] === 'flash') {
+          $(this).parent().children().removeClass('flash'); //supposed to remove flash class
+        }
+        // $(this).parent().children().addClass('flash'); //adds flash class
+        i++;
+      }
     }
   });
 
