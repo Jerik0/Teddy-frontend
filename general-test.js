573e8feb235a6bb5b87b67ae7a06b2acf83b5786
Jeriko <jcarrico1985@live.com>
2018-01-31 12:25:45 -0600

    created function allowing for staggering casing of string characters. alphabetized function arrangement.

--- general-test.js
+++ general-test.js
@@ -2,12 +2,29 @@
 
 // (() => {
 
-  const containsCapitalLetter = (str) => {
-    if(str===str.toLowerCase()) {
-      console.log('Capitals not included');
-    } else {
-      console.log('Capitals included');
+
+  const addDigit = () => {
+    for(let i=0; i<10; i++) {
+      let result = '';
+      for(let j=0; j<i; j++) {
+        result += i;
+      }
+      console.log(result);
     }
+  };
+
+  const betweenTheLines = (str) => {
+    let badWords = ['pony', 'unicorn', 'bleh', 'poop', 'smeg'];
+    for(let i=0; i<badWords.length; i++) {
+      for(let j=0; j<str.length; j++) {
+        //check each char in str against this index of badWords. if the char is in this iteration, add it to a new array. if the new array
+      }
+    }
+  };
+
+  const capitalize = (str) => {
+    let firstChar = str.substring(0,1).toUpperCase();
+    return firstChar + str.substring(1);
   };
 
   const cleanString = (arr) => {
@@ -21,48 +38,8 @@
     return nastyArr.join('');
   };
 
-  const openConesShop = () => {
-    let allCones = Math.floor(Math.random() * 50) + 50;
-
-    do {
-      let randomCones = Math.floor(Math.random() * 5 + 1);
-      if(allCones < randomCones) {
-        allCones -= allCones;
-        console.log("Cones left: " + allCones);
-      } else {
-        allCones -= randomCones;
-        console.log("Cones left: " + allCones);
-      }
-    } while (allCones > 0);
-  };
-
-  const multiplesTable = (num, max) => {
-    for (let i=0; i<=max; i++) {
-      let result = num * i;
-      console.log(num + ' * ' + i + ' = ' + result);
-    }
-  };
-
-/*
-* TODO write a function that takes in a string and checks for characters in the string that could be used for any one word in an array of words. If so, print out those words that were spelled with the characters from the string. */
-
-  const betweenTheLines = (str) => {
-    let badWords = ['pony', 'unicorn', 'bleh', 'poop', 'smeg'];
-    for(let i=0; i<badWords.length; i++) {
-      for(let j=0; j<str.length; j++) {
-        //check each char in str against this index of badWords. if the char is in this iteration, add it to a new array. if the new array
-      }
-    }
-  };
-
-  const addDigit = () => {
-    for(let i=0; i<10; i++) {
-      let result = '';
-      for(let j=0; j<i; j++) {
-        result += i;
-      }
-      console.log(result);
-    }
+  const containsCapitalLetter = (str) => {
+    return str===str.toLowerCase();
   };
 
   const fizzBuzz = () => {
@@ -80,20 +57,7 @@
   };
 
   const isEven = (num) => {
-    return (parseInt(num) % 2 == 0);
-  };
-
-  for(let i=0; i<10; i++) {
-    let num = Math.floor((Math.random() * 20) + 200);
-    isEven(num);
-  }
-
-  const reverseInput = (str) => {
-    let reversed = '';
-    for(let i=str.length; i>-1; i--) {
-      reversed += str.charAt(i);
-    }
-    return reversed;
+    return (parseInt(num) % 2==0);
   };
 
   const isPalindrome = (str) => {
@@ -113,6 +77,60 @@
       half2 = half2.join('');
     }
     return (half === half2);
+  };
+
+
+  const multiplesTable = (num, max) => {
+    for (let i=0; i<=max; i++) {
+      let result = num * i;
+      console.log(num + ' * ' + i + ' = ' + result);
+    }
+  };
+
+  const numGenerator = () => {
+    for(let i=0; i<10; i++) {
+      let num = Math.floor((Math.random() * 20) + 200);
+      isEven(num);
+    }
+  };
+
+  const openConesShop = () => {
+    let allCones = Math.floor(Math.random() * 50) + 50;
+
+    do {
+      let randomCones = Math.floor(Math.random() * 5 + 1);
+      if(allCones < randomCones) {
+        allCones -= allCones;
+        console.log("Cones left: " + allCones);
+      } else {
+        allCones -= randomCones;
+        console.log("Cones left: " + allCones);
+      }
+    } while (allCones > 0);
+  };
+
+/*
+* TODO write a function that takes in a string and checks for characters in the string that could be used for any one word in an array of words. If so, print out those words that were spelled with the characters from the string. */
+
+  const reverseInput = (str) => {
+    let reversed = '';
+    for(let i=str.length; i>-1; i--) {
+      reversed += str.charAt(i);
+    }
+    return reversed;
+  };
+
+  const staggerString = (str) => {
+    let newArray = [];
+    for(let i=0; i<str.length; i++) {
+      if(isEven(i)) {
+        newArray.push(str[i].toUpperCase());
+      } else {
+        newArray.push(str[i]);
+      }
+    }
+    newArray = newArray.join('');
+    return newArray;
   };
 
 // })();
